3d5f68e70d12a1cfa2a0c907ecbb58f139a1c327
LeonardoRd <gaitanleonardo6@gmail.com>
2023-12-11 20:20:46 -0300

    Arreglos visuales

--- client/src/components/NavbarOne.jsx
+++ client/src/components/NavbarOne.jsx
@@ -1,25 +1,29 @@
 import { Link } from 'react-router-dom'
 import { useAuth } from '../context/authContext'
 import { useState } from 'react'
+import { SlArrowDown } from 'react-icons/sl'
+import { IoMdExit } from 'react-icons/io'
+import { IoAddCircleSharp } from 'react-icons/io5'
+import { SiWelcometothejungle } from 'react-icons/si'
 
 function NavbarOne() {
   const { isAuthenticate, user } = useAuth()
   const [showOptions, setShowOptions] = useState(false)
+  const [showProducts, setShowProducts] = useState(false)
+  const [showTipoComp, setShowTipoComp] = useState(false)
+  const [showFacturas, setShowFacturas] = useState(false)
   const [showOptionsTipo, setShowOptionsTipo] = useState(false)
 
-  const handleToggleOptions = () => {
-    setShowOptions(true)
-  }
-  const handleToggleOptions2 = () => {
-    setShowOptions(false)
+  const setOptionFacturas = () => {
+    setShowFacturas(!showFacturas)
   }
 
-  const handleToggleOptionsTipo = () => {
-    setShowOptionsTipo(true)
+  const setOptionTipoCompr = () => {
+    setShowTipoComp(!showTipoComp)
   }
 
-  const handleToggleOptionsTipo2 = () => {
-    setShowOptionsTipo(false)
+  const setOptionProducts = () => {
+    setShowProducts(!showProducts)
   }
 
   return (
@@ -33,34 +37,55 @@ function NavbarOne() {
       {isAuthenticate && (
         <ul className="flex flex-col items-center text-center md:flex-row gap-x-2">
           {/* Mensaje del usuario logueado*/}
-          <li className="font-bold px-4 py-2 w-full h-full">Bienvenido {user.username}</li>
+          <div className='inline-flex justify-between justify-items-center'>
+            
+            <li className="font-bold px-4 py-2 w-full h-full">
+              Bienvenido {user.username}
+            </li>
+          </div>
 
           {/* Agregar Tareas */}
           <li className="rounded-md bg-zinc-600 px-4 py-2 w-full h-full text-center hover:bg-zinc-500">
-            <Link className="w-auto h-[2rem]" to="/add-task">
-              Agregar Tareas
-            </Link>
+            <div className="inline-flex justify-between justify-items-center">
+              <IoAddCircleSharp className="w-10 h-10" />
+              <Link className="w-auto h-[2rem]" to="/add-task">
+                Agregar Tareas
+              </Link>
+            </div>
           </li>
 
           {/* Agregar Facturas */}
           <li
-            className={`relative block md:inline-block rounded-md bg-zinc-600 px-3 py-2 w-full h-full text-center hover:bg-zinc-500 ${
-              showOptions ? 'active' : ''
+            className={`relative block md:inline-block rounded-md bg-zinc-600 px-3 py-2 w-full h-full text-center hover:bg-zinc-500 
             }`}
           >
-            <div className="w-full h-full" onMouseEnter={handleToggleOptions}>
-              <Link className="w-full h-[2rem]"to="/invoices">Facturas</Link>
+            <div
+              onClick={setOptionFacturas}
+              className="w-full h-full inline-flex justify-items-center text-center"
+            >
+              <SlArrowDown className="w-5 h-5 mt-2 mr-2 p-0" />
+              <button className="ml-2 text-center">Facturas</button>
             </div>
 
             {/* Menu desplegable de Facturas */}
-            {showOptions && (
+            {showFacturas && (
               <ul className="absolute left-0 mt-2 items-center w-100">
-                <li
-                  onMouseEnter={handleToggleOptions}
-                  onMouseLeave={handleToggleOptions2}
-                  className="block px-4 bg-zinc-600 py-2 hover:bg-zinc-500 w-auto h-auto"
-                >
-                  <Link className="w-full h-[2rem]"to="/add-invoice">
+                <li className="block px-4 bg-zinc-600 py-2 hover:bg-zinc-500 w-auto h-auto">
+                  <Link
+                    onClick={setOptionFacturas}
+                    className="w-full h-[2rem]"
+                    to="/invoices"
+                  >
+                    Ver Facturas
+                  </Link>
+                </li>
+
+                <li className="block px-4 bg-zinc-600 py-2 hover:bg-zinc-500 w-auto h-auto">
+                  <Link
+                    onClick={setOptionFacturas}
+                    className="w-full h-[2rem]"
+                    to="/add-invoice"
+                  >
                     Agregar Facturas
                   </Link>
                 </li>
@@ -70,45 +95,91 @@ function NavbarOne() {
 
           {/* Agregar Tipos de Comprobantes */}
           <li
-            className={`relative block md:inline-block rounded-md text-center w-full h-auto bg-zinc-600 px-3 py-2 hover:bg-zinc-500 ${
-              showOptions ? 'active' : ''
+            className={`relative block md:inline-block rounded-md text-center w-full h-auto bg-zinc-600 px-3 py-2 hover:bg-zinc-500
             }`}
           >
             <div
-              onMouseEnter={handleToggleOptionsTipo}
-              className="w-full text-center"
+              onClick={setOptionTipoCompr}
+              className="w-full h-full inline-flex justify-between justify-items-center"
             >
-              <Link className="w-full h-[2rem]" to="/tiposComprob">Tipos de Comprobantes</Link>
+              <SlArrowDown className="w-10 h-5" />
+              <button>Tipos de Comprobantes</button>
             </div>
 
             {/* Menu desplegable de Tipos de Comprobantes */}
-            {showOptionsTipo && (
+            {showTipoComp && (
               <ul className="absolute left-0 mt-2">
-                <li
-                  onMouseEnter={handleToggleOptionsTipo}
-                  onMouseLeave={handleToggleOptionsTipo2}
-                  className="block px-4 bg-zinc-600 py-2 hover:bg-zinc-500 w-auto"
-                >
-                  <Link className="w-full h-[2rem]" to="/add-tipoComprob">
+                <li className="block px-4 bg-zinc-600 py-2 hover:bg-zinc-500 w-auto">
+                  <Link
+                    onClick={setOptionTipoCompr}
+                    className="w-full h-[2rem]"
+                    to="/tiposComprob"
+                  >
+                    Tipos de Comprobantes
+                  </Link>
+                </li>
+                <li className="block px-4 bg-zinc-600 py-2 hover:bg-zinc-500 w-auto">
+                  <Link
+                    onClick={setOptionTipoCompr}
+                    className="w-full h-[2rem]"
+                    to="/add-tipoComprob"
+                  >
                     Agregar Tipos de Comprobantes
                   </Link>
                 </li>
-                <li
-                  onMouseEnter={handleToggleOptionsTipo}
-                  onMouseLeave={handleToggleOptionsTipo2}
-                  className="block px-4 bg-zinc-600 py-2 w-full hover:bg-zinc-500"
-                >
-                  <Link className="w-full h-[2rem]" to="/estados">
+                <li className="block px-4 bg-zinc-600 py-2 w-full hover:bg-zinc-500">
+                  <Link
+                    onClick={setOptionTipoCompr}
+                    className="w-full h-[2rem]"
+                    to="/estados"
+                  >
                     Estados de Comprobantes
                   </Link>
                 </li>
-                <li
-                  onMouseEnter={handleToggleOptionsTipo}
-                  onMouseLeave={handleToggleOptionsTipo2}
-                  className="block px-4  bg-zinc-600 py-2 w-full hover:bg-zinc-500"
-                >
-                  <Link className="w-full h-[2rem]" to="/add-estado">
+                <li className="block px-4  bg-zinc-600 py-2 w-full hover:bg-zinc-500">
+                  <Link
+                    onClick={setOptionTipoCompr}
+                    className="w-full h-[2rem]"
+                    to="/add-estado"
+                  >
                     Agregar Estados Comprobantes
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </li>
+
+          {/* Agregar Productos */}
+          <li
+            className={`relative block md:inline-block rounded-md bg-zinc-600 px-3 py-2 w-full h-full text-center hover:bg-zinc-500}`}
+          >
+            <div
+              onClick={setOptionProducts}
+              className="w-full h-full inline-flex justify-between justify-items-center"
+            >
+              <SlArrowDown className="mt-2 w-10 h-5" />
+              <button>Productos</button>
+            </div>
+
+            {/* Menu desplegable de Productos */}
+            {showProducts && (
+              <ul className="absolute left-0 mt-2 items-center w-auto">
+                <li className="block px-4 bg-zinc-600 py-2 hover:bg-zinc-500 w-auto h-auto">
+                  <Link
+                    onClick={setOptionProducts}
+                    className="w-full h-[2rem]"
+                    to="/productos"
+                  >
+                    Ver Productos
+                  </Link>
+                </li>
+                <li className="block px-4 bg-zinc-600 py-2 hover:bg-zinc-500 w-auto h-auto">
+                  <Link
+                    onClick={setOptionProducts}
+                    className="w-full h-[2rem]"
+                    to="/add-productos"
+                  >
+                    Agregar Productos
                   </Link>
                 </li>
               </ul>
@@ -117,9 +188,12 @@ function NavbarOne() {
 
           {/* SALIR */}
           <li className="rounded-md bg-zinc-600 px-4 py-2 w-full text-center hover:bg-zinc-500">
-            <Link className="w-full h-[2rem]" to="/logout">
-              Salir
-            </Link>
+            <div className="flex justify-between align-bottom">
+              <Link className="w-full h-[2rem]" to="/logout">
+                Salir
+              </Link>
+              <IoMdExit className="w-10 h-10" />
+            </div>
           </li>
         </ul>
       )}
